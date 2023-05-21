4dae3aefe7eb98d469960bc5998bc95761790ef1
AWI195 <111362664+AWI195@users.noreply.github.com>
2023-05-21 16:35:35 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
+//Calculating square area
 function hitungLuas(){
     var sisiLuas = document.getElementById("sisi-luas").value;
     if(Number(sisiLuas)){
-        document.getElementById("output_luas").innerHTML = '<br>L = S x S<br>L = '+sisiLuas+' x '+sisiLuas+'<br>L = '+sisiLuas**2;
+        document.getElementById("output_luas").innerHTML = '<br>L = S x S<br>L = '+sisiLuas+' x '+sisiLuas+'<br>L = '+sisiLuas**2;//If the input is number then dispalay square area calculation
     } else {
-        document.getElementById("output_luas").innerHTML = "This is not number";
+        document.getElementById("output_luas").innerHTML = "This is not number";//If the input is not number then display this massage
     }
-    document.getElementById("sisi-luas").value = "";
+    document.getElementById("sisi-luas").value = "";//Clear the value of the input
 }
+//Reset calculation output
 function resetLuas(){
     document.getElementById("output_luas").innerHTML = "";
     document.getElementById("sisi-luas").value = "";
 }
-//Keliling
+//Calculating the perimeter of a square
 function hitungKeliling(){
     var sisiLuas = document.getElementById("sisi-keliling").value;
     if(Number(sisiLuas)){
-        document.getElementById("output_keliling").innerHTML = '<br>K = 4 x S<br>K = 4 x '+sisiLuas+'<br>K = '+sisiLuas*4;
+        document.getElementById("output_keliling").innerHTML = '<br>K = 4 x S<br>K = 4 x '+sisiLuas+'<br>K = '+sisiLuas*4;//If the input is number then dispalay square circumference calculation
     } else {
-        document.getElementById("output_keliling").innerHTML = "This is not number";
+        document.getElementById("output_keliling").innerHTML = "This is not number";//If the input is not number then display this massage
     }
-    document.getElementById("sisi-keliling").value = "";
+    document.getElementById("sisi-keliling").value = "";//Clear the value of the input
 }
+//Reset calculation output
 function resetKeliling(){
     document.getElementById("output_keliling").innerHTML = "";
     document.getElementById("sisi-keliling").value = "";
